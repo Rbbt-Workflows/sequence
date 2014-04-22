59d44a0fe6dfe42011fd28f5d26780757b3720c6
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-04-22 16:55:40 +0200

    nil

--- test_workflow.rb
+++ test_workflow.rb
@@ -23,5 +23,23 @@ class TestSequence < Test::Unit::TestCase
       assert proteins.reject{|p| p.empty?}.empty? || m.split(":")[2] == "?"
     end
   end
+
+  def test_mutated_isoforms_10_000_stream
+    step = Sequence.example_step('mutated_isoforms', '10_000')
+    step = step.recursive_clean.run true
+    mis = TSV.open step 
+    toffs = step.step(:transcript_offsets).load
+    organism = step.info[:inputs][:organism]
+    missing  = toffs.keys - mis.keys
+
+    index = Organism.transcripts(organism).index :target => "Ensembl Protein ID", :source => "Ensembl Transcript ID", :persist => true
+    missing.each do |m|
+      proteins = toffs[m].collect do |to|
+        t,o,*rest = to.split ":"
+        index[t]
+      end
+      assert proteins.reject{|p| p.empty?}.empty? || m.split(":")[2] == "?"
+    end
+  end
 end
 
