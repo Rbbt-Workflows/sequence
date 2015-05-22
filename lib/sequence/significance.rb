4ead0959bc08f83878911eb692cd6683c1d241f7
Miguel Vazquez <miguel.vazquez@cnio.es>
2015-05-22 18:09:50 +0200

    Fix support for recurrent mutations

--- lib/sequence/significance.rb
+++ lib/sequence/significance.rb
@@ -2,16 +2,37 @@ require 'rbbt/util/R'
 
 module Sequence
 
-  dep :affected_genes
   input *MUTATIONS_INPUT
-  task :binomial_significance => :tsv do |mutations|
-    mutations = mutations.join.load if Step === mutations
-    mutation_count = Array === mutations ? Misc.counts(mutations.collect{|m| m.split(":").values_at(0,1)*":"}) : Misc.counts(Open.read(mutations).split("\n").collect{|m| m.split(":").values_at(0,1)*":"})
-    iif mutation_count
-    genes_step = step(:affected_genes)
-    organism = genes_step.step(:mutated_isoforms_fast).inputs[1]
+  input *ORGANISM_INPUT
+  input :exome, :boolean, "Limit analysis to exome bases", true
+  dep do |jobname,options|
+    if options[:exome]
+      Sequence.job(:affected_genes, jobname, :organism => options[:organism], :mutations => options[:mutations])
+    else
+      Sequence.job(:genes, jobname, :organism => options[:organism], :positions => options[:mutations])
+    end
+  end
+  task :binomial_significance => :tsv do |mutations, organism, exome|
+    if Step === mutations
+      Step.wait_for_jobs dependencies + [mutations]
+      mutations = mutations.load 
+    else
+      Step.wait_for_jobs dependencies
+    end
+
+    if exome
+      genes_step = step(:affected_genes)
+      organism = genes_step.step(:mutated_isoforms_fast).inputs[:organism]
+    else
+      genes_step = step(:genes)
+      organism = genes_step.inputs[:organism]
+    end
 
     tsv = TSV.setup({}, :key_field => "Ensembl Gene ID", :fields => ["Matches", "Bases", "Frequency"], :namespace => organism, :type => :list, :cast => :to_f)
+
+    mutation_count = Array === mutations ? 
+      Misc.counts(mutations.collect{|m| m.split(":").values_at(0,1)*":"}) : 
+      Misc.counts(Open.read(mutations).split("\n").collect{|m| m.split(":").values_at(0,1)*":"})
 
     genes = Set.new
     mutations = Set.new
@@ -27,25 +48,32 @@ module Sequence
       end
     end
 
+    mutations = mutations.to_a
     genes = genes.to_a
 
-    #num_mutations = mutations.to_a.length
     num_mutations = mutation_count.values.inject(0){|acc,e| acc += e; acc}
 
-    total_bases = Organism.gene_list_exon_bases(genes)
-    global_frequency = num_mutations.to_f / total_bases
+    if exome
+      total_bases = Organism.gene_list_exon_bases(genes)
+      global_frequency = num_mutations.to_f / total_bases
 
-    gene2exon_size = Misc.process_to_hash(genes){|genes| genes.collect{|gene| Organism.gene_list_exon_bases([gene]) }}
+      gene2size = Misc.process_to_hash(genes){|genes| genes.collect{|gene| Organism.gene_list_exon_bases([gene]) }}
+    else
+      total_bases = Organism.gene_list_bases(genes)
+      global_frequency = num_mutations.to_f / total_bases
+
+      gene2size = Misc.process_to_hash(genes){|genes| genes.collect{|gene| Organism.gene_list_bases([gene]) }}
+    end
 
     TSV.traverse genes, :bar => "Computing significance" do |gene|
       mutations = gene_mutations[gene].uniq
       next if mutations.empty?
       matches = mutations.collect{|mutation| mutation_count[mutation]}.inject(0){|acc,e| acc += e}
-      exon_bases = gene2exon_size[gene]
-      next if exon_bases == 0
-      frequency = matches.to_f / exon_bases
+      bases = gene2size[gene]
+      next if bases == 0
+      frequency = matches.to_f / bases
 
-      tsv[gene] = [matches, exon_bases, frequency]
+      tsv[gene] = [matches, bases, frequency]
     end
 
     begin
@@ -85,20 +113,27 @@ module Sequence
     genes = genes.to_a
     num_mutations = mutations.to_a.length
 
-    total_bases = Organism.gene_list_exon_bases(genes)
-    global_frequency = num_mutations.to_f / total_bases
+    if exome
+      total_bases = Organism.gene_list_bases(genes)
+      global_frequency = num_mutations.to_f / total_bases
 
-    gene2exon_size = Misc.process_to_hash(genes){|genes| genes.collect{|gene| Organism.gene_list_exon_bases([gene]) }}
+      gene2size = Misc.process_to_hash(genes){|genes| genes.collect{|gene| Organism.gene_list_bases([gene]) }}
+    else
+      total_bases = Organism.gene_list_bases(genes)
+      global_frequency = num_mutations.to_f / total_bases
+
+      gene2size = Misc.process_to_hash(genes){|genes| genes.collect{|gene| Organism.gene_list_bases([gene]) }}
+    end
 
     TSV.traverse genes, :bar => "Computing significance" do |gene|
       mutations = gene_mutations[gene].uniq
       next if mutations.empty?
       matches = mutations.length
-      exon_bases = gene2exon_size[gene]
-      next if exon_bases == 0
-      frequency = matches.to_f / exon_bases
+      bases = gene2size[gene]
+      next if bases == 0
+      frequency = matches.to_f / bases
 
-      tsv[gene] = [matches, exon_bases, frequency]
+      tsv[gene] = [matches, bases, frequency]
     end
 
     new = tsv.R "
