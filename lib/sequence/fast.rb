069bcc11530f44165703bdcd700cc8ea3bf53c0f
Miguel Vazquez <miguel.vazquez@cnio.es>
2015-09-30 12:17:00 +0200

    nil

--- lib/sequence/fast.rb
+++ lib/sequence/fast.rb
@@ -26,7 +26,7 @@ module Sequence
     dumper = TSV::Dumper.new :key_field => "Genomic Mutation", :fields => ["Mutated Isoform"], :type => :flat, :namespace => organism
     dumper.init
 
-    TSV.traverse mutations, :cpus => 3, :bar => self.progress_bar("Mutated Iso. Fast"), :into => dumper, :type => :array do |mutation|
+    TSV.traverse mutations, :cpus => 1, :bar => self.progress_bar("Mutated Iso. Fast"), :into => dumper, :type => :array do |mutation|
       next if mutation.nil?
       chr, pos, mut_str = mutation.split(":")
       next if mut_str.nil?
